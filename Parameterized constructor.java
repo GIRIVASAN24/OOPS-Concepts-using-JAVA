308f26eef7c64426ec75e2d8494384b52a6a2149
GIRIVASAN M V <147172260+GIRIVASAN24@users.noreply.github.com>
2024-12-30 11:25:28 +0530

    Update Parameterized constructor.java

--- Parameterized constructor.java
+++ Parameterized constructor.java
@@ -1,1 +1,19 @@
+class Parameterized{
+int number;
+String name;
+public Parameterized(int number,String name){
+this.number=number;
+this.name=name;
+}
+public void display(){
+System.out.println("Number : "+number+" "+"Name : "+name);
+}
+}
+class Mains
+{
+public static void main(String args[]){
+Parameterized obj=new Parameterized(067,"Harirajan");
+obj.display();
+}
+}
 
